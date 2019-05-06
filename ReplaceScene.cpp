365e7fa6f9084f60a5c317379b505efbd29f112e
NineGoodMan <710098335@qq.com>
2019-05-06 17:13:51 +0800

    Create ReplaceScene.cpp

---
+++ ReplaceScene.cpp
@@ -0,0 +1,24 @@
+//	TransitionJumpZoom
+//	Create a bouncing transition animation
+//		auto action = TransitionJumpZoom ::create(time , scene);
+//		Director::getInstance()->replaceScene(action);
+
+//	TransitionProgressRadialCCW
+//	Create a fanbar style transition animation, counterclockwise
+//		auto action = TransitionProgressRadialCCW::create(time, scene);
+//		Director:: getInstance()->replaceScene(action);
+
+//	TransitionProgressRadialCW
+//	Create a fanbar style transition animation, clockwise
+//		auto action = TransitionProgressRadialCW::create(time,scene);
+//		Director:: getInstance()->replaceScene(action); 
+
+//	TransitionProgressHorizontal
+//	Create a horizontal bar style transition animation
+//		auto action = TransitionProgressHorizontal ::create(time,scene);
+//		Director:: getInstance()->replaceScene(action);
+
+//	TransitionProgressVertical
+//	Create a vertical bar in the form of transition animations
+//		auto action = TransitionProgressVertical::create(time, scene);
+//		Director:: getInstance()->replaceScene(action);
