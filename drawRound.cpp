d1a8519530872d479eecc1ec5aa1b6dddb86aef4
NineGoodMan <710098335@qq.com>
2019-05-06 17:09:35 +0800

    Create drawRound.cpp

---
+++ drawRound.cpp
@@ -0,0 +1,10 @@
+/*
+* @brief        画?角矩形    
+* @param        origin            矩形?始点
+* @param        destination        矩形?束点
+* @param        radius            ?角半径
+* @param        segments        ?角等?数，等?越多，?角越平滑
+* @param        bFill            是否填充
+* @param        color            填充?色
+* @attention        
+*/
